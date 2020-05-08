8f00c8b1d425e4ea0fc26e2e132e4096565990df
fwcd <fwcdmail@gmail.com>
2020-05-08 03:59:51 +0200

    Implement basic CLI with readline

--- src/client.ts
+++ src/client.ts
@@ -2,13 +2,9 @@ import * as proto from "socket-chat-protocol";
 import { EventEmitter } from "events";
 import WebSocket from "ws";
 
-// TODO: Figure out how to strongly type the 'on' function without causing
-//       rest arguments in the listener's parameters to be incorrectly detected
-//       as an array by TypeScript.
-
-// export declare interface Client {
-//     on<K extends keyof proto.Client.Event>(event: "ready" | K, listener: (...params: proto.Client.Event[K][]) => void): this;
-// }
+export declare interface Client {
+    on<K extends keyof proto.Client.Event>(event: "ready" | K, listener: (...params: proto.Client.Event[K]) => void): this;
+}
 
 /** The socket chat client. */
 export class Client extends EventEmitter {
@@ -31,7 +27,7 @@ export class Client extends EventEmitter {
         });
     }
     
-    send<K extends keyof proto.Server.Event>(method: K, ...params: proto.Server.Event[K][]) {
+    send<K extends keyof proto.Server.Event>(method: K, params: proto.Server.Event[K]) {
         this.ws.send(JSON.stringify({ method, params }));
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as readline from "readline";
 import { Client } from "./client";
 import { ArgumentParser } from "argparse";
 
@@ -20,6 +21,7 @@ parser.addArgument(["-a", "--address"], {
 async function main(): Promise<void> {
     const args = parser.parseArgs();
     const client = new Client(args.address);
+
     client.on("ready", () => {
         console.log(`Logging in as ${args.username}...`);
         client.send("server/login-request", [{
@@ -27,14 +29,30 @@ async function main(): Promise<void> {
             password: args.password
         }]);
     });
+
     client.on("server/login-response", resp => {
         if (resp.success) {
             console.log("Ready!");
-            client.on("channel/send-message", msgs => {
+            client.on("channel/send-message", (...msgs) => {
                 for (const msg of msgs) {
                     console.log(`<${msg.author}@${msg.channel}> ${msg.content}`);
                 }
             });
+            
+            const cli = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+            
+            cli.on("line", line => {
+                client.send("channel/send-message", [{
+                    author: "dummy", // TODO: This is deprecated, but not optional
+                    channel: "default",
+                    content: line
+                }]);
+                cli.prompt();
+            });
+            cli.prompt();
         } else {
             console.log(`Login was unsuccessful: ${JSON.stringify(resp)}`);
         }
